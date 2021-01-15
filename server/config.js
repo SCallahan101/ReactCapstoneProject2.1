6f17ecbe219c059bf2c6176c1d8c21c8b4ffbbdc
Shannon Callahan <Shannonc619@gmail.com>
2021-01-15 11:21:19 -0700

    Fix up with new mongo atlas

--- server/config.js
+++ server/config.js
@@ -1,4 +1,6 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || "mongodb://ReactUser123:ReactPw123@ds259207.mlab.com:59207/react-cap-project-db";
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || "mongodb+srv://ReactUser123:ReactPw123@react-cap-project-db.ay47g.mongodb.net/react-cap-project-db?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 4747;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'JwtReactCapstone';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+
+// "mongodb://ReactUser123:ReactPw123@ds259207.mlab.com:59207/react-cap-project-db"
